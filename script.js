ec90aaf068d659343fa66bcc8243ffb10b884556
Murilo Quartz <153753008+Muriloxox@users.noreply.github.com>
2025-01-11 23:48:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+const starCount = 300;
+const body = document.body;
+
+for (let i = 0; i < starCount; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    const delay = Math.random() * 5;
+
+    star.style.left = `${x}px`;
+    star.style.top = `${y}px`;
+    star.style.animationDelay = `${delay}s`;
+
+    body.appendChild(star);
+}
+
+const astroName = document.getElementById('astro-name');
+
+const astroNamesMap = {
+    sun: 'Sol',
+    venus: 'Vênus',
+    neptune: 'Netuno',
+    urano: 'Urano',
+    saturn: 'Saturno',
+    jupiter: 'Júpiter',
+    mercury: 'Mercúrio',
+    mars: 'Marte',
+    earth: 'Terra'
+};
+
+const planets = document.querySelectorAll('.sun, .venus, .neptune, .urano, .saturn, .jupiter, .mercury, .mars, .earth');
+
+planets.forEach(planet => {
+    planet.addEventListener('mouseover', () => {
+        const planetClass = [...planet.classList].find(cls => astroNamesMap[cls]);
+        if (planetClass) {
+            astroName.textContent = astroNamesMap[planetClass];
+            astroName.style.display = 'block';
+        }
+    });
+
+    planet.addEventListener('mouseout', () => {
+        astroName.style.display = 'none';
+    });
+});
+
 
